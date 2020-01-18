2785e097e4e0e97474f324e33060afc12985755e
Michael Henretty <michael.henretty@gmail.com>
2020-01-17 22:48:55 -0800

    better formatting

--- go.py
+++ go.py
@@ -2,14 +2,14 @@ import test
 from test import this as from_this
 import test.this as test_this
 
-print("--just test--")
+print("\n-- import test--")
 print(test)
 
-print("import test, then test.this")
+print("\n-- import test, then test.this")
 print(test.this)
 
-print("from test import this")
+print("\n-- from test import this")
 print(from_this)
 
-print("imoprt this.test")
+print("\n-- import this.test")
 print(test_this)
